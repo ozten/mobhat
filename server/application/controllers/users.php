af4879a2d4889fdc30fa3db48ed6f32d41f80e59
Ozten <shout@ozten.com>
2009-02-11 01:48:04 -0500

    Added users/whoami. Added what-page-is-this-oface Ubiquity command

---
+++ server/application/controllers/users.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Users_Controller extends Controller
+{
+    private $auth;
+    private $user;
+    private $session;
+    
+    public function __construct()
+    {
+        // load database library into $this->db (can be omitted if not required)
+        parent::__construct();
+        
+        $this->auth = new Auth();
+        $this->session = Session::instance();
+    }
+    
+    public function whoami()
+    {
+        $this->auth->auto_login();
+        if (! $this->auth->logged_in()) {
+            $this->session->set("requested_url","/". url::current() );
+            url::redirect('/auth_demo/login_form');
+        } else {			
+            $this->user = Session::instance()->get('auth_user');            
+            $this->auto_render = FALSE;
+            
+            $this->facetModel = new Facet_Model;
+            echo json_encode(array(
+                'id'       => $this->user->id,
+                'username' => $this->user->username,
+                'facets'   => $this->facetModel->current_facets($this->user->username)
+                             ));
+        }
+    }
+}
+?>
---
+++ server/modules/auth/views/auth_demo/success.php
@@ -0,0 +1,2 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<h1>Success</h1>
