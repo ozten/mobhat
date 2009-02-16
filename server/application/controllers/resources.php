143434e71fb656351799813b76ef241349cd6ee5
Ozten <shout@ozten.com>
2009-02-16 15:01:42 -0500

    Changed /resources/query_facets to handle multiple users and created resources/user/{username}/query_facets, for single user info

--- server/application/controllers/resources.php
+++ server/application/controllers/resources.php
@@ -30,59 +30,125 @@ class Resources_Controller extends Template_Controller
     public $template = 'kohana/template';
     /**
     $.ajax({
-       url: '/resources/query_facets',
+        url: '/resources/query_facets',
         type: 'POST',
         dataType: 'json',
-        data: "q=" + JSON.stringify([{url: escape('http://example.com/'), updated: '2009-01-28T06:00:29Z'},{url: escape('http://example.com/art.jpg'), updated: '2009-01-27T13:22:21Z'}]),
+        data: "q=" + JSON.stringify({urls: [{username: 'ozten', url: escape('http://example.com/'), published: '2009-01-28T06:00:29Z'},
+                                            {username: 'pattyok', url: escape('http://example.com/art.jpg'), published: '2009-01-27T13:22:21Z'}]}),
         success: function(json, status){ console.info(json); }
      });
      */
     public function query_facets()
     {
-        Kohana::log('info', "query_facets() " . request::method() );
+
         $this->auto_render=false;
+        $msg = "";
         if (request::method() == "post") {
             
+            $post = $this->input->post();            
+            $query = json_decode($post['q'], true);            
+            $origItems = $query['urls'];
+            
+            //TODO url_decode each url before using...
+            $items_by_author = $this->_prepareItemsByAuthor($origItems);
+            $this->_facetItemsAnAuthorAtATime($items_by_author, $origItems);
+            echo json_encode($origItems);
+        } else {
+            echo json_encode(array('error' => "Unknown action, expecting POST"));
+        }
+    }
+    
+    /**
+    $.ajax({
+        url: '/resources/user/ozten/query_facets',
+        type: 'POST',
+        dataType: 'json',
+        data: "q=" + JSON.stringify({urls: [{url: escape('http://example.com/'), published: '2009-01-28T06:00:29Z'},{url: escape('http://example.com/art.jpg'), published: '2009-01-27T13:22:21Z'}]}),
+        success: function(json, status){ console.info(json); }
+     });
+     */
+    public function user($username, $action)
+    {
+        $this->auto_render=false;
+        if (request::method() == "post" && $action == "query_facets") {
             $post = $this->input->post();
-            Kohana::log('info', Kohana::debug($post['q']));
+            
             
             $query = json_decode($post['q'], true);
             $items = $query['urls'];
             //TODO url_decode each url before using...
-            Kohana::log('info', Kohana::debug($items));
-            
-            $username = $query['username'];
-            Kohana::log('info', $username);
-            Kohana::log('info', Kohana::debug($username));
+                            
             $userId = $this->_userId($username);
             if( $userId <= 0 ) {
                 //TODO if request is for one unknown user... send a 404
-                header('http_response_code', true, 404);
+                header('http_response_code', true, 404);                
             } else {
                 $currentFacets = $this->facetDb->current_facets($username);
-            
-                foreach($items as $i => $item){
-                    $date = $item['published'];                
-                    $facets = $this->facetDb->facetsDuring($userId, $date);
-                    if (count($facets) > 0) {
-                        $items[$i]['facets'] = $facets;
-                    } else {
-                        $items[$i]['facets'] = $currentFacets;
-                    }
-                }
+                $this->_updateFacetInfo($userId, $currentFacets, $items);                
             }
+            //TODO wrap with in an object {urls: $items, user: {profile: url}} etc
             echo json_encode($items);
-        } else {
-            echo json_encode(array('error' => "Unknown action, expecting POST"));
+             
         }
-        
-        
     }
     private function _userId($username)
     {
         //$model = ORM::factory('user')->find(11);//'username', $username);
         $model = ORM::factory('user')->where('username', 'pattyok')->find();        
         return $model->id;
+    }
+    private function _updateFacetInfo($userId, $currentFacets, &$items)
+    {
+        foreach($items as $i => $item){            
+            $date = $item['published'];                
+            $facets = $this->facetDb->facetsDuring($userId, $date);
+            if (count($facets) > 0) {
+                $items[$i]['facets'] = $facets;
+            } else {
+                $items[$i]['facets'] = $currentFacets;
+            }
+        }
+    }
+    
+    private function _prepareItemsByAuthor($items)
+    {
+        $items_by_author = array();
+        foreach($items as $i => $item){
+            $item['index'] = $i;
+            if ( ! array_key_exists('username', $item)) {
+                $m = "Error, no username set in item at index $i";
+                Kohana::log('alert', $m);                    
+            } else {
+                $username = $item['username'];
+                if ( ! array_key_exists($username, $items_by_author)) {
+                    $items_by_author[$username] = array();
+                }
+                array_push($items_by_author[$username], $item);
+            }
+        }
+        return $items_by_author;
+    }
+    
+    private function _facetItemsAnAuthorAtATime(&$items_by_author, &$origItems)
+    {
+        foreach ($items_by_author as $username => $items) {
+            $userId = $this->_userId($username);
+            if( $userId <= 0 ) {
+                $s = "WARNING: Query contains an unknown username $username id: $userId\n";
+                $msg += $s;
+                Kohana::log('alert', $s);
+            } else {
+                //TODO include link to profile and current facets for each user?
+                $currentFacets = $this->facetDb->current_facets($username);            
+                $this->_updateFacetInfo($userId, $currentFacets, $items_by_author[$username]);
+                foreach($items_by_author[$username] as $i => $facetedItem) {
+                    $index = $facetedItem['index'];
+                    if( array_key_exists('facets', $facetedItem)) {
+                        $origItems[$index]['facets'] = $facetedItem['facets'];
+                    }
+                }
+            }    
+        }
     }
 }
 ?>
