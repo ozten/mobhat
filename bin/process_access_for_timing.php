ad6871ffb68dca297722976839407282348b21f2
Ozten <shout@ozten.com>
2009-03-30 06:46:36 -0700

    Adding quick script for process_access_for_timing

---
+++ bin/process_access_for_timing.php
@@ -0,0 +1,14 @@
+<?php
+$pipe = fopen("php://stdin", "r");
+      $thedata = "";
+      while ($data = fgets($pipe)){
+            //^[^[]*\[([^]]*)\] "GET /static/images/timing.gif\?s1=(\d*)&s..=(\d*)&s..=(\d*)&s..=(\d*)&s..=(\d*)&s..=(\d*) HTTP/.*$
+            
+            echo preg_replace('/^[^[]*\\[([^]]*)\\] "GET \/static\/images\/timing.gif\\?s1=(\\d*)&s..=(\\d*)&s..=(\\d*)&s..=(\\d*)&s..=(\\d*)&s..=(\\d*) HTTP\/.*$/',
+                              "\\1, \\2, \\3, \\4, \\5, \\6, \\7\n", $data);
+            //$thedata = $thedata . $data . "EOL";
+      }
+//echo $thedata;
+//echo "\n";
+
+?>
