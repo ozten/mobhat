58bceecac6341751f57e534c9e99ff461293970b
Ozten <shout@ozten.com>
2009-03-30 19:01:58 -0700

    Turning onPageLoadon

--- server/static/ubiquity/mobhat.js
+++ server/static/ubiquity/mobhat.js
@@ -1386,12 +1386,12 @@ function ofaceToggler(){
 /**
  * You can use fetch- command  during development (comment out pageLoad_fetchFeedOface )
  * or uncomment pageLoad_fetchFeedOface for auto load
-
+*/
 function pageLoad_fetchFeedOface(){
     CmdUtils.log("Starting ");
     ofaceObj.preview.call(ofaceObj);
 }
-*/  
+  
 
 ;(function(){
 CmdUtils.CreateCommand(ofaceObj);
