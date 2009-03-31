742013f1b16edc362eafc73aab88d0bdec7679cf
Ozten <shout@ozten.com>
2009-03-30 19:14:00 -0700

    Switching to prod image urls

--- server/static/ubiquity/mobhat.js
+++ server/static/ubiquity/mobhat.js
@@ -17,7 +17,7 @@ CmdUtils.onPageLoad(function(){
   background-color: #DDD;\
 }\
 .group-facet span.count{\
-  background-image: url(http://oface.ubuntu/static/images/ubiquity/BowlerIcon39x27.png);\
+  background-image: url(http://mobhat.restservice.org/static/images/ubiquity/BowlerIcon39x27.png);\
   background-repeat: no-repeat;\
   display: block;\
   width: 39px;\
@@ -168,15 +168,15 @@ Oface.Models.ResourceDB = {
 Oface.Views = Oface.Views || {};
 Oface.Views.userFacetToggler =
 <div><h3 id='oface-enabler' style='float: left' title='Click to Change'>
-        <img src="http://oface.ubuntu/static/images/ubiquity/mobhat-logo-sm.png" width="93" height="17" title="MOBhat" /> is
+        <img src="http://mobhat.restservice.org/static/images/ubiquity/mobhat-logo-sm.png" width="93" height="17" title="MOBhat" /> is
     <span class='status'>Enabled</span>
-    <img src='http://oface.ubuntu/static/images/dell_icon-power-button.gif'
+    <img src='http://mobhat.restservice.org/static/images/dell_icon-power-button.gif'
          style='vertical-align: bottom' width='16' height='16' title="Power Switch for MOBHat" />
                                
     </h3>
                               
     <div id="facet-toggler" class='current-facet' ><div style="margin-top:18px; margin-left: 3px; float:left"></div>
-        <div class='switcher-arrow' style='margin-top:20px; float: left; height:6px; width:7px; margin-left:3px; font-size:0; vertical-align:middle; background:transparent url(http://oface.ubuntu/static/images/gmail_downarros.png) no-repeat scroll -36px 50%;'> x</div>
+        <div class='switcher-arrow' style='margin-top:20px; float: left; height:6px; width:7px; margin-left:3px; font-size:0; vertical-align:middle; background:transparent url(http://mobhat.restservice.org/static/images/gmail_downarros.png) no-repeat scroll -36px 50%;'> x</div>
     </div>
 </div>.children();; //TODO restructure this...
 var Oface = Oface || {};
@@ -773,7 +773,7 @@ Oface.Controllers.Facet = Oface.Controllers.Facet || {
                             var offset = switcher.offset();
                             Oface.Views.Facet.hideAll();
                             $('#oface-enabler', doc).after(
-                                '<div id="switcher-progress-panel"><img src="http://oface.ubuntu/static/images/ubiquity/progress-icon.gif" /></div>');
+                                '<div id="switcher-progress-panel"><img src="http://mobhat.restservice.org/static/images/ubiquity/progress-icon.gif" /></div>');
                             var p = $('#switcher-progress-panel', doc).width(w).height(h).offset(offset);
                         },
                         complete: function(){$('#switcher-progress-panel', doc).remove(); },
